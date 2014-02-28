4cda4e8072e0bc5ef03f1778c54f4b4f591cef00
Uwe Hermann <uwe@hermann-uwe.de>
2014-02-28 13:17:48 +0100

    new-driver: Drop unneeded comment.

--- source/drv-protocol.h
+++ source/drv-protocol.h
@@ -25,7 +25,6 @@
 #include "libsigrok.h"
 #include "libsigrok-internal.h"
 
-/* Message logging helpers with subsystem-specific prefix string. */
 #define LOG_PREFIX "${short}"
 
 /** Private, per-device-instance driver context. */
