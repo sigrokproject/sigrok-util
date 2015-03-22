87604b0c3ec0115a89921039ff12a6fa4606ddcb
Aurelien Jacobs <aurel@gnuage.org>
2015-03-23 00:26:44 +0100

    new-driver: Update to match recent drivers API changes.

--- source/drv-api.c
+++ source/drv-api.c
@@ -20,14 +20,13 @@
 #include "protocol.h"
 
 SR_PRIV struct sr_dev_driver ${lib}_driver_info;
-static struct sr_dev_driver *di = &${lib}_driver_info;
 
-static int init(struct sr_context *sr_ctx)
+static int init(struct sr_dev_driver *di, struct sr_context *sr_ctx)
 {
 	return std_init(sr_ctx, di, LOG_PREFIX);
 }
 
-static GSList *scan(GSList *options)
+static GSList *scan(struct sr_dev_driver *di, GSList *options)
 {
 	struct drv_context *drvc;
 	GSList *devices;
@@ -44,12 +43,12 @@ static GSList *scan(GSList *options)
 	return devices;
 }
 
-static GSList *dev_list(void)
+static GSList *dev_list(const struct sr_dev_driver *di)
 {
 	return ((struct drv_context *)(di->priv))->instances;
 }
 
-static int dev_clear(void)
+static int dev_clear(const struct sr_dev_driver *di)
 {
 	return std_dev_clear(di, NULL);
 }
@@ -76,9 +75,9 @@ static int dev_close(struct sr_dev_inst *sdi)
 	return SR_OK;
 }
 
-static int cleanup(void)
+static int cleanup(const struct sr_dev_driver *di)
 {
-	dev_clear();
+	dev_clear(di);
 
 	/* TODO: free other driver resources, if any. */
 
